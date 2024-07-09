c252089bdb15dad3c563a69b34e697e4c94f5766
AkshadAp17 <148571478+AkshadAp17@users.noreply.github.com>
2024-07-09 19:00:20 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import createaplloserver from "./graphql";
 
 async function init() {
   const app = express();
-  const PORT = 8000;
+  
   app.use(express.json());
 
   app.get("/", (req, res) => {
